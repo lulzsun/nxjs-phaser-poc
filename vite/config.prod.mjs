d786fb99d412469e4e019feab4cd0c5545c0d2ee
Jimmy Quach <jminquach@gmail.com>
2024-06-15 23:34:24 -0700

    build filename without format

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -38,7 +38,7 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, '../src/main.ts'),
             name: 'nxjs-phaser-poc',
-            fileName: 'nxjs',
+            fileName: () => { return 'nxjs.js' },
             formats: ['esm'],
         },
         minify: false,
