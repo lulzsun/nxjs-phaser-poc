461de4a913ddcbd22fd6d552bc79a725725fef9b
Jimmy Quach <jminquach@gmail.com>
2024-06-18 07:02:03 -0700

    refactor NXImageFile.ts
    ok wtf i could've just used Image(), seriously??? this is even faster than what i was previously doing 🤦

--- src/plugins/loader/filetypes/NXImageFile.ts
+++ src/plugins/loader/filetypes/NXImageFile.ts
@@ -1,9 +1,8 @@
-import { ChannelOrder, decodePng } from 'image-in-browser';
 import { name } from '../../../../package.json';
 
 export class NXImageFile extends Phaser.Loader.FileTypes.ImageFile {
     declare cache: Phaser.Textures.TextureManager;
-    declare data: OffscreenCanvas;
+    declare data: HTMLImageElement;
 
     constructor(
         loader: Phaser.Loader.LoaderPlugin, 
@@ -12,62 +11,32 @@ export class NXImageFile extends Phaser.Loader.FileTypes.ImageFile {
         xhrSettings?: Phaser.Types.Loader.XHRSettingsObject,
         frameConfig?: Phaser.Types.Loader.FileTypes.ImageFrameConfig
     ) {
+        loader.imageLoadType = 'HTMLImageElement';
         super(loader, key, url, xhrSettings, frameConfig);
     }
 
-    load(): void {
-        if (this.state === Phaser.Loader.FILE_POPULATED) {
-            //  Can happen for example in a JSONFile if they've provided a JSON object instead of a URL
-            this.loader.nextFile(this, true);
-        }
-        else {
-            this.state = Phaser.Loader.FILE_LOADING;
+    loadImage(): void {
+        this.state = Phaser.Loader.FILE_LOADING;
 
-            this.src = Phaser.Loader.GetURL(this, this.loader.baseURL);
+        this.src = 'sdmc:/switch/' + name + '/' + this.url;
 
-            if (!this.src) {
-                throw new Error('URL Error in File: ' + this.key + ' from: ' + this.url);
-            }
+        this.data = new Image();
 
-            if (this.src.indexOf('data:') === 0) {
-                this.base64 = true;
-            }
+        // this.data.crossOrigin = this.crossOrigin;
 
-            if (Switch === undefined) {
-                this.xhrLoader = Phaser.Loader.XHRLoader(this, this.loader.xhr);
-                return;
-            }
+        var _this = this;
 
-            this.onProcess();
-        }
-    }
+        this.data.onload = function () {
+            _this.state = Phaser.Loader.FILE_LOADED;
 
-    onProcess(): void {
-        if (Switch === undefined) {
-            super.onProcess();
-            return;
-        }
-        this.state = Phaser.Loader.FILE_PROCESSING;
+            _this.loader.nextFile(_this, true);
+        };
 
-        Switch.readFile('sdmc:/switch/' + name + '/' + this.url)
-            .then((buffer) => {
-                const img = decodePng({ data: buffer as Uint8Array })!;
-        
-                this.data = new OffscreenCanvas(img.width, img.height);
-                const ctx = this.data.getContext('2d')!;
-                
-                // Creating ImageData from MemoryImage
-                const imageData = ctx.createImageData(img.width, img.height);
-                const rawBytes = img.getBytes({ order: ChannelOrder.rgba });
-                imageData.data.set(rawBytes);
+        this.data.onerror = function ()
+        {
+            _this.loader.nextFile(_this, false);
+        };
 
-                // Putting ImageData into context
-                ctx.putImageData(imageData, 0, 0);
-                this.onProcessComplete();
-
-                this.state = Phaser.Loader.FILE_LOADED;
-                this.loader.nextFile(this, false);
-            }
-        );
+        this.data.src = this.src;
     }
 }
